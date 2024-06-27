e3b12fa211094e0e8b6b61debc8ef834f1573073
elhamy786 <elham.afzali1383@gmail.com>
2024-06-27 22:18:34 +0430

    Solve script.js errors once again

--- script.js
+++ script.js
@@ -23,21 +23,21 @@ document.getElementById('convert-btn').addEventListener('click', function() {
 });
 
 function convertToRoman(num) {
-    const romanNumerals = [
-    ["M", 1000],
-    ["CM", 900],
-    ["D", 500],
-    ["CD", 400],
-    ["C", 100],
-    ["XC", 90],
-    ["L", 50],
-    ["XL", 40],
-    ["X", 10],
-    ["IX", 9],
-    ["V", 5],
-    ["IV", 4],
-    ["I", 1]
-    ];
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
     let result = '';
 
